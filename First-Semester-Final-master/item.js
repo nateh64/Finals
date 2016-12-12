07ddd4081ee79170c24617482090af735882a767
nateh64 <7nhuzenis@stpatrick.org>
2016-12-12 13:55:18 -0600

    Add files via upload

---
+++ First-Semester-Final-master/item.js
@@ -0,0 +1,13 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor (n ,p, s){
+    this.name= n;
+    this.price= p;
+    this.shipping= s;
+  }
+
+
+
+}
+
+//Create your three test items below this comment.
