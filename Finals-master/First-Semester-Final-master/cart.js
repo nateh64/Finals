5ad6f17fafe554c36b632c9a3db85dcdb1fb76e3
nateh64 <7nhuzenis@stpatrick.org>
2016-12-14 14:04:27 -0600

    Add files via upload

---
+++ Finals-master/First-Semester-Final-master/cart.js
@@ -0,0 +1,14 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+  constructor(il,iq){
+    this.itemList=il;
+    this.itemQuantity=iq;
+  }
+
+
+  //Type the instance functions below this comment.
+  addItem(i,q){
+    
+  }
+
+}
---
+++ Finals-master/First-Semester-Final-master/item.js
@@ -0,0 +1,15 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor (n ,p, s){
+    this.name= n;
+    this.price= p;
+    this.shipping= s;
+  }
+this.jersey=new Item("jersey", 30, 10);
+this.hoodie=new Item("hoodie", 50, 5);
+this.hat=new Item("hat", 30, 0);
+
+
+}
+
+//Create your three test items below this comment.
