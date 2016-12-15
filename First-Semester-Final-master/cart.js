d2db033a4937e2be207b2db5461a4e439e751d42
nateh64 <7nhuzenis@stpatrick.org>
2016-12-15 13:54:16 -0600

    Add files via upload

--- First-Semester-Final-master/cart.js
+++ First-Semester-Final-master/cart.js
@@ -1,9 +1,35 @@
 class Cart{
   //What is the first part of every class? Type it below.
-
+  constructor(il,iq){
+    this.itemList=il;
+    this.itemQuantity=iq;
+  }
 
 
   //Type the instance functions below this comment.
+  addItem(i,q){
+    this.itemList.push(i);
+    this.itemQuantity.push(q);
+  }
+  totalCart(){
+    let total=0
+    for(let t=0; t<=this.itemList.length; t++){
+      total +=this.itemList[t].price * this.itemQuantity[t]
+    }
+    return total
 
+  }
+  subCart(days){
+    let subCart=[];
+    for(let s=0;s<this.itemList.length;s++){
+      if(this.itemList[s].shipping==days){
+        subCart.push(this.itemList[s]);
+      }
+      return subCart;
+    }
+  }
+
+
+  }
 
 }
--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -1,10 +1,21 @@
 class Password{
   //The first part of the class.
-
+constructor(pri,pub){
+  this.privatekey=pri;
+  this.publickey=pub;
+}
 
 
   //Instance functions below this comment.
-
+validPublicKey(){
+  if(this.publickey>=8 && this.publickey<=25){
+    return true
+  }
+  else(false)
+}
+validPrivateKey(){
+  
+}
 
 
   //Static function below this comment.
