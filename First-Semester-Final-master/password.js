049b47d4949ef93540fc36cece17f65e481851a1
nateh64 <7nhuzenis@stpatrick.org>
2016-12-16 13:54:28 -0600

    Add files via upload

--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -13,10 +13,26 @@ validPublicKey(){
   }
   else(false)
 }
+
+
+}
 validPrivateKey(){
-  
+  if(this.privatekey[4]="_" && this.privatekey[9]='_'){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 
   //Static function below this comment.
+static makePrivateKey(){
+  let key='';
+  let limit= 14;
+  for(let n=0; n<limit; n++){
+    if (!n==4 && !==9){
+      key.push(Math.floor(Math.random()*10))
+  }
+  console.log(key);
 }
